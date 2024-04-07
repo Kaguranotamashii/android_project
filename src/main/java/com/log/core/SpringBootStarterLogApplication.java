fdeb99402dfb8a13a021726b8ef94f9dcb46e9a9
宋宇然 <5592286+song_yu_ran@user.noreply.gitee.com>
2024-04-07 09:05:20 +0800

    提交

---
+++ src/main/java/com/log/core/SpringBootStarterLogApplication.java
@@ -0,0 +1,16 @@
+package com.log.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
+@EnableAsync
+@SpringBootApplication
+public class SpringBootStarterLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterLogApplication.class, args);
+    }
+
+}
